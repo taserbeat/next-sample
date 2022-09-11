3b1e86ac489f119c9e6aa4714166d3c7112081d6
taserbeat <taserbeat@gmail.com>
2022-09-11 20:24:05 +0900

    feat: サンプルページを作成

---
+++ pages/sample.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import React from 'react'
+
+const Sample: NextPage = () => {
+    return (
+        <span>サンプルのページです</span>
+    )
+}
+
+export default Sample
